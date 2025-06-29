1ac40398240954089bad38575c6820ae17077891
adeliafitri <adeliafitri972@gmail.com>
2025-06-29 15:56:48 +0700

    [fix] edit footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2023 Program Studi <a href="https://arsitektur.uin-malang.ac.id/">Arsitektur</a></strong>
-    UIN Malang.
+    &copy; 2023 Program Studi Arsitektur UIN Maulana Malik Ibrahim Malang - Produk ini digunakan oleh Teknik Kimia Polinema malalui perjanjian resmi.
     {{-- <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 3.2.0
     </div> --}}
